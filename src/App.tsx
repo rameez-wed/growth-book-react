ad30677c6113caaaf4ee5bcc36d95d944e468808
Rameez Khan <rameez@wednesday.is>
2022-07-27 18:00:18 +0530

    add growthbook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { GrowthBook, GrowthBookProvider } from '@growthbook/growthbook-react'
 
-function App() {
+function App () {
+  // Create a GrowthBook instance
+  const growthbook = new GrowthBook({
+    // Callback when a user is put into an A/B experiment
+    trackingCallback: (experiment, result) => {
+      console.log('Experiment Viewed', {
+        experimentId: experiment.key,
+        variationId: result.variationId
+      })
+    }
+  })
+
+  useEffect(() => {
+    growthbook.setAttributes({
+      id: 'foo',
+      deviceId: 'foo',
+      company: 'foo',
+      loggedIn: true,
+      employee: true,
+      country: 'foo',
+      browser: 'foo',
+      url: 'foo'
+    })
+
+    // Use a feature!
+
+    const FEATURES_ENDPOINT =
+      'http://localhost:3100/api/features/key_prod_e052f703237661ec'
+    fetch(FEATURES_ENDPOINT)
+      .then(res => res.json())
+      .then(json => {
+        console.log('json is', json)
+        growthbook.setFeatures(json.features)
+      })
+      .catch(() => {
+        console.log('Failed to fetch feature definitions from GrowthBook')
+      })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <GrowthBookProvider growthbook={growthbook}>
+      <div className='App'>
+        <p>Hi there!</p>
+      </div>
+    </GrowthBookProvider>
+  )
 }
 
-export default App;
+// TODO: replace with real targeting attributes
+
+export default App
